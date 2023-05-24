61bc63a88725e5dc710647fa23177f05e725bbe6
ksan09 <kansan060223@gmail.com>
2023-05-25 00:39:17 +0900

    cameraSetting

--- 2023_(1)PJ/Assets/01.Scripts/Player/PlayerBrain.cs
+++ 2023_(1)PJ/Assets/01.Scripts/Player/PlayerBrain.cs
@@ -1,115 +1,59 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerBrain : MonoBehaviour
 {
-    public enum State
-    {
-        None,
-        Move,
-        Jump,
-        SkillMove,
-    }
-
-    private PlayerInput _playerInput;   // 입력
-    private PlayerSkill _playerSkill;   // 스킬
-    private Movement _movement;         // 이동
-
-    private State _state = State.None;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    [SerializeField]
+    private float playerSpeed = 2.0f;
+    [SerializeField]
+    private float jumpHeight = 1.0f;
+    [SerializeField]
+    private float gravityValue = -9.81f;
+    private InputManager inputManager;
+    private Transform cameraTransform;
 
     private void Awake()
     {
-        _playerInput    = GetComponent<PlayerInput>();
-        _playerSkill    = GetComponent<PlayerSkill>();
-        _movement       = GetComponent<Movement>();
-
+        controller = gameObject.GetComponent<CharacterController>();
+        cameraTransform = Camera.main.transform;
     }
 
     private void Start()
     {
-        InitPlayer();
+        inputManager = InputManager.Instance;
     }
 
-    private void Update()
+    void Update()
     {
-        MovePlayer();
-        JumpPlayer();
-        RotatePlayer();
-        PlayerSkill();
-
-        PlayerState();
-        DebugPlayer();
-    }
-
-
-    private void InitPlayer()
-    {
-        // 커서 안 보이게
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-
-       
-    }
-    private void MovePlayer()
-    {
-        _movement.MoveDir = Vector3.zero;
-
-        if (_playerInput.Forward)
-            _movement.MoveDir += transform.forward;
-        if (_playerInput.Left)
-            _movement.MoveDir += -transform.right;
-        if (_playerInput.Back)
-            _movement.MoveDir += -transform.forward;
-        if (_playerInput.Right)
-            _movement.MoveDir += transform.right;
-
-        _movement.Move();
-    }
-    private void JumpPlayer()
-    {
-        //
-        if (_playerInput.Jump && GroundCheck())
-            _movement.Jump();
-
-    }
-    private void RotatePlayer()
-    {
-        Vector3 rotate = Vector3.zero;
-        rotate.y = Define.MainCam.transform.eulerAngles.y;
-
-        _movement.Rotate(rotate);
-    }
-    private void PlayerSkill()
-    {
-        if(_playerInput.ShotWeb)
-            _playerSkill.ShotWeb();
-        else if(_playerInput.PoolWeb)
-            _playerSkill.PoolWeb();
-    }
-    private void PlayerState()
-    {
-        // 현재 플레이어 상황을 봐준다
-        _state = State.None; // 상황 초기화
-
-        if (_playerSkill.IsUseSkill) // 스킬 사용 상태 확인
-            _state = State.SkillMove;
-        else if(_movement.MoveDir != Vector3.zero) // 멈춰있지 않다면
-            _state = State.Move;
-    }
-
-    private void DebugPlayer()
-    {
-        if(Input.GetKeyDown(KeyCode.L))
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
         {
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
-        }    
-    }
-    private bool GroundCheck()
-    {
-        // 적어둬야 함
-        return true;
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 movement = inputManager.GetPlayerMovement();
+        Vector3 move = new Vector3(movement.x, 0f, movement.y);
+        move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
+        move.y = 0;
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (inputManager.PlayerJumpedThisFrame() && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
--- 2023_(1)PJ/Assets/01.Scripts/Player/PlayerSkill.cs
+++ 2023_(1)PJ/Assets/01.Scripts/Player/PlayerSkill.cs
@@ -4,96 +4,95 @@ using UnityEngine;
 
 public class PlayerSkill : MonoBehaviour
 {
-    private LineRenderer    _webLine;       // 거미줄 그려주기
-    [SerializeField]
-    private LayerMask       _shotAbleLayer; // 거미줄이 설치 가능한 레이어
-    [SerializeField]
-    private float           _webDist = 5;   // 거미줄 길이
+    //[SerializeField]
+    //private LayerMask       _shotAbleLayer; // 거미줄이 설치 가능한 레이어
+    //[SerializeField]
+    //private float           _webDist = 5;   // 거미줄 길이
 
-    private RaycastHit  hit;
-    private Vector3     _hitPos;            // 레이캐스트 맞은 위치
-    [SerializeField]
-    private Transform   _webPos;            // 거미줄 발사 위치
-    public bool IsUseSkill { get; private set; }
+    //private RaycastHit  hit;
+    //private Vector3     _hitPos;            // 레이캐스트 맞은 위치
+    //[SerializeField]
+    //private Transform   _webPos;            // 거미줄 발사 위치
+    //public bool IsUseSkill { get; private set; }
 
-    private SpringJoint _joint;
-    private Movement _movement;
+    //private SpringJoint _joint;
+    //private Movement _movement;
 
-    private void Awake()
-    {
-        _webLine = GetComponent<LineRenderer>();
-        _movement = GetComponent<Movement>();
-        DrawWeb(false);
-    }
+    //private void Awake()
+    //{
+    //    _webLine = GetComponent<LineRenderer>();
+    //    _movement = GetComponent<Movement>();
+    //    DrawWeb(false);
+    //}
 
-    private void Update()
-    {
-        RenderWeb();
-    }
+    //private void Update()
+    //{
+    //    RenderWeb();
+    //}
 
-    private void RenderWeb()
-    {
-        if (IsUseSkill == false) return;
+    //private void RenderWeb()
+    //{
+    //    if (IsUseSkill == false) return;
 
-        _webLine.SetPosition(0, _webPos.position);
-        _webLine.SetPosition(1, _hitPos);
-    }
+    //    _webLine.SetPosition(0, _webPos.position);
+    //    _webLine.SetPosition(1, _hitPos);
+    //}
 
-    private void DrawWeb(bool isDraw)
-    {
-        IsUseSkill = isDraw;
-        _webLine.enabled = isDraw;
-    }
+    //private void DrawWeb(bool isDraw)
+    //{
+    //    IsUseSkill = isDraw;
+    //    _webLine.enabled = isDraw;
+    //}
 
-    public void ShotWeb()
-    {
-        CancelWeb();
-        Debug.Log("레이저 발사");
-        // 레이저를 쏴
-        bool isHit = (Physics.Raycast(Define.MainCam.transform.position,
-            Define.MainCam.transform.forward, out hit, _webDist, _shotAbleLayer));
+    //public void ShotWeb()
+    //{
+    //    CancelWeb();
+    //    Debug.Log("레이저 발사");
+    //    // 레이저를 쏴
+    //    bool isHit = (Physics.Raycast(Define.MainCam.transform.position,
+    //        Define.MainCam.transform.forward, out hit, _webDist, _shotAbleLayer));
 
-        // 맞으면 Pos를 저장
-        if(isHit)
-        {
-            Debug.Log("목표 맞음");
-            _hitPos = hit.point;
+    //    // 맞으면 Pos를 저장
+    //    if(isHit)
+    //    {
+    //        Debug.Log("목표 맞음");
+    //        _hitPos = hit.point;
 
-            // 라인 렌더러 그리기 
-            DrawWeb(true);
+    //        // 라인 렌더러 그리기 
+    //        DrawWeb(true);
 
-            // 조인트 생성
-            _joint = gameObject.AddComponent<SpringJoint>();
-            _joint.autoConfigureConnectedAnchor = false;
-            _joint.connectedAnchor = _hitPos;
+    //        // 조인트 생성
+    //        _joint = gameObject.AddComponent<SpringJoint>();
+    //        _joint.autoConfigureConnectedAnchor = false;
+    //        _joint.connectedAnchor = _hitPos;
 
-            float dist = Vector3.Distance(transform.position, _hitPos);
+    //        float dist = Vector3.Distance(transform.position, _hitPos);
 
-            _joint.maxDistance = dist;
-            _joint.minDistance = dist * 0.5f;
-            _joint.spring = 5f;
-            _joint.damper = 5f;
-            _joint.massScale = 5f;
-        }
+    //        _joint.maxDistance = dist;
+    //        _joint.minDistance = dist * 0.5f;
+    //        _joint.spring = 5f;
+    //        _joint.damper = 5f;
+    //        _joint.massScale = 5f;
+    //    }
 
         
-    }
+    //}
 
-    public void PoolWeb()
-    {
-        if (_joint == null) return;
-        // 라인 렌더러 지우기
-        CancelWeb();
-        // hitPos 방향으로 AddForce
-        Vector3 dir = _hitPos - transform.position;
-        float dist = Vector3.Distance(transform.position, _hitPos);
-        _movement.Dash(dir.normalized, dist * 1.8f, 0.55f);
-    }
+    //public void PoolWeb()
+    //{
+    //    if (_joint == null) return;
+    //    // 라인 렌더러 지우기
+    //    CancelWeb();
+    //    // hitPos 방향으로 AddForce
+    //    Vector3 dir = _hitPos - transform.position;
+    //    float dist = Vector3.Distance(transform.position, _hitPos);
+    //    _movement.Dash(dir.normalized, dist * 1.8f, 0.55f);
+    //}
 
-    public void CancelWeb()
-    {
-        // 라인 렌더러 지우기
-        DrawWeb(false);
-        Destroy(_joint);
-    }
+    //public void CancelWeb()
+    //{
+    //    // 라인 렌더러 지우기
+    //    DrawWeb(false);
+    //    Destroy(_joint);
+    //}
 }
